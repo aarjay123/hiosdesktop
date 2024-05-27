f409523304df950c15dc1134ac2e037ed7de30e7
aarjay123 <76173037+aarjay123@users.noreply.github.com>
2024-05-27 20:07:51 +0100

    Fixed a few issues with the help page

--- HiOSDesktop/ViewModels/HelpViewModel.cs
+++ HiOSDesktop/ViewModels/HelpViewModel.cs
@@ -1,18 +1,110 @@
-﻿using System.Reflection;
-using System.Windows.Input;
-
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 using HiOSDesktop.Contracts.Services;
-using HiOSDesktop.Helpers;
+using HiOSDesktop.Contracts.ViewModels;
 
-using Microsoft.UI.Xaml;
-
-using Windows.ApplicationModel;
+using Microsoft.Web.WebView2.Core;
 
 namespace HiOSDesktop.ViewModels;
 
-public partial class HelpViewModel : ObservableRecipient
+// TODO: Review best practices and distribution guidelines for WebView2.
+// https://docs.microsoft.com/microsoft-edge/webview2/get-started/winui
+// https://docs.microsoft.com/microsoft-edge/webview2/concepts/developer-guide
+// https://docs.microsoft.com/microsoft-edge/webview2/concepts/distribution
+public partial class HelpViewModel : ObservableRecipient, INavigationAware
 {
+    // TODO: Set the default URL to display.
+    [ObservableProperty]
+    private Uri source = new("https://thehighlandcafe.github.io/hioswebcore/desktop/help.html");//TODO: MAKE HIOSWEBCORE PAGES, DEPLOY THIS
+
+    [ObservableProperty]
+    private bool isLoading = true;
+
+    [ObservableProperty]
+    private bool hasFailures;
+
+    public IWebViewService WebViewService
+    {
+        get;
+    }
+
+    public HelpViewModel(IWebViewService webViewService)
+    {
+        WebViewService = webViewService;
+    }
+
+    [RelayCommand]
+    private async Task OpenInBrowser()
+    {
+        if (WebViewService.Source != null)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(WebViewService.Source);
+        }
+    }
+
+    [RelayCommand]
+    private void Reload()
+    {
+        WebViewService.Reload();
+    }
+
+    [RelayCommand(CanExecute = nameof(BrowserCanGoForward))]
+    private void BrowserForward()
+    {
+        if (WebViewService.CanGoForward)
+        {
+            WebViewService.GoForward();
+        }
+    }
+
+    private bool BrowserCanGoForward()
+    {
+        return WebViewService.CanGoForward;
+    }
+
+    [RelayCommand(CanExecute = nameof(BrowserCanGoBack))]
+    private void BrowserBack()
+    {
+        if (WebViewService.CanGoBack)
+        {
+            WebViewService.GoBack();
+        }
+    }
+
+    private bool BrowserCanGoBack()
+    {
+        return WebViewService.CanGoBack;
+    }
+
+    public void OnNavigatedTo(object parameter)
+    {
+        WebViewService.NavigationCompleted += OnNavigationCompleted;
+    }
+
+    public void OnNavigatedFrom()
+    {
+        WebViewService.UnregisterEvents();
+        WebViewService.NavigationCompleted -= OnNavigationCompleted;
+    }
+
+    private void OnNavigationCompleted(object? sender, CoreWebView2WebErrorStatus webErrorStatus)
+    {
+        IsLoading = false;
+        BrowserBackCommand.NotifyCanExecuteChanged();
+        BrowserForwardCommand.NotifyCanExecuteChanged();
+
+        if (webErrorStatus != default)
+        {
+            HasFailures = true;
+        }
+    }
+
+    [RelayCommand]
+    private void OnRetry()
+    {
+        HasFailures = false;
+        IsLoading = true;
+        WebViewService?.Reload();
+    }
 }
